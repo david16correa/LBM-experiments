b573659b50e5b7002df0d8225d06b54a852917d5
David Correa <david.correa.bsc@gmail.com>
2024-08-16 15:53:36 -0600

    cleanup

--- readmeFigs/main.jl
+++ readmeFigs/main.jl
@@ -2,6 +2,7 @@
 =============================================================================================
 
 goal:
+
 to generate some nice figures for the repos
 
 =============================================================================================
@@ -12,8 +13,7 @@ using Pkg; Pkg.activate(".");
 using LBMengine
 norm(V) = sum(el for el in V.*V) |> sqrt
 
-len = 100;
-x = range(-1, stop = 1, length = len);
+x = range(-1, stop = 1, length = 101);
 solidNodes = [
     #= ((-0.75 < i < -0.25) && j < 0.75) || ((0.25 < i < 0.75) && j > -0.75) # big walls =#
     #= ((-0.75 < i < -0.25) && j < 0.) || ((0.25 < i < 0.75) && j > -0.) # small walls =#
@@ -22,13 +22,11 @@ for i in x, j in x];
 
 model = modelInit(; 
     x = x,
-    Δt = :default, # default: Δt = Δx
-    relaxationTimeRatio = 0.8, # τ/Δt > 1 → under-relaxation, τ/Δt = 1 → full relaxation, 0.5 < τ/Δt < 1 → over-relaxation, τ/Δt < 0.5 → unstable
+    relaxationTimeRatio = 0.8, # τ/Δt
     walledDimensions = [2],
     solidNodes = solidNodes,
     forceDensity = [1.0e-3, 0.0],
     isFluidCompressible = true,
-    forcingScheme = :guo # {:guo, :shan}
 );
 
 @time LBMpropagate!(model; simulationTime = 15, verbose = true);
@@ -37,6 +35,7 @@ model = modelInit(;
 #=@time plotMomentumDensity(model);=#
 model.fluidVelocity / model.fluidParams.c_s .|> norm |> maximum # Mach Number
 model.schemes
+
 @time anim8massDensity(model; verbose = true);
 @time anim8fluidVelocity(model; verbose = true);
 #=@time anim8momentumDensity(model);=#
