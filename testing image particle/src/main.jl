3bb0ea4ccd8cfc616c47e5b2faa7eb236508c642
David Correa <david.correa.bsc@gmail.com>
2024-09-26 16:41:09 -0600

    this test now compares Ladd and PSM

--- testing image particle/src/main.jl
+++ testing image particle/src/main.jl
@@ -11,12 +11,18 @@ using Pkg; Pkg.activate(".");
 #= ] dev ../../LBMengine.jl =#
 
 # packages
-using LBMengine, CairoMakie, Dates, LinearAlgebra
+using LBMengine
 
 include("src/params.jl")
 
-outputDir = "data.lbm/movingParticle"
-mkdir(outputDir)
+#= ==========================================================================================
+=============================================================================================
+Ladd
+=============================================================================================
+========================================================================================== =#
+
+outputDir = "data.lbm/ladd/movingParticle"
+run(`mkdir -p $outputDir`)
 
 #= ==========================================================================================
 =============================================================================================
@@ -38,6 +44,7 @@ addBead!(model;
     coupleTorques = coupleTorques,
     coupleForces = coupleForces,
     angularVelocity = angularVelocity,
+    scheme = :ladd
 );
 
 @time LBMpropagate!(model; simulationTime = simulationTime, verbose = true);
@@ -67,6 +74,7 @@ addBead!(modelClose2wall;
     coupleTorques = coupleTorques,
     coupleForces = coupleForces,
     angularVelocity = angularVelocity,
+    scheme = :ladd
 );
 
 @time LBMpropagate!(modelClose2wall; simulationTime = simulationTime, verbose = true);
@@ -77,76 +85,69 @@ mv("output.lbm", "$(outputDir)/R")
 
 #= ==========================================================================================
 =============================================================================================
-I - partícula imagen
+PSM
 =============================================================================================
 ========================================================================================== =#
 
-component = 1
-fig = Figure();
-ax = Axis(fig[1,1], title = "x-component of velocity, t = $(modelClose2wall.time |> x -> round(x; digits = 2)), x = $(modelClose2wall.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "u_x";
-modelClose2wall.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "VR");
-model.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, model.spaceTime.x, v, label = "VL");
-modelClose2wall.fluidVelocity[desiredXid, :] - model.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "VR - VL = VI");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
+outputDir = "data.lbm/psm/movingParticle"
+run(`mkdir -p $outputDir`)
 
 #= ==========================================================================================
 =============================================================================================
-figs
+L - lejos de la pared
 =============================================================================================
 ========================================================================================== =#
 
-#= julia> findfirst(x -> x >= 0, model.spaceTime.x) =#
-#= 101 =#
-desiredXid = 101
+model = modelInit(;
+    x = x,
+    relaxationTimeRatio = relaxationTimeRatio,
+    walledDimensions = walledDimensions,
+    isFluidCompressible = isFluidCompressible,
+    saveData = true
+);
+addBead!(model;
+    massDensity = massDensity,
+    radius = radius,
+    position = position,
+    coupleTorques = coupleTorques,
+    coupleForces = coupleForces,
+    angularVelocity = angularVelocity,
+    scheme = :psm
+);
 
-# L - lejos de la pared
-fig = Figure();
-ax = Axis(fig[1,1], title = "speed, t = $(model.time |> x -> round(x; digits = 2)), x = $(model.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "|u|";
-model.fluidVelocity[desiredXid, :] .|> norm |> v -> lines!(ax, model.spaceTime.x, v, label = "fluid velocity");
-model.particles[1].nodeVelocity[desiredXid, :] .|> norm |> v -> lines!(ax, model.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
-component = 1
-fig = Figure();
-ax = Axis(fig[1,1], title = "x-component of velocity, t = $(model.time |> x -> round(x; digits = 2)), x = $(model.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "u_x";
-model.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, model.spaceTime.x, v, label = "fluid velocity");
-model.particles[1].nodeVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, model.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
-component = 2
-fig = Figure();
-ax = Axis(fig[1,1], title = "y-component of velocity, t = $(model.time |> x -> round(x; digits = 2)), x = $(model.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "u_y ";
-model.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, model.spaceTime.x, v, label = "fluid velocity");
-model.particles[1].nodeVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, model.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
+@time LBMpropagate!(model; simulationTime = simulationTime, verbose = true);
+@time plotMassDensity(model);
+@time plotFluidVelocity(model);
 
-# R - cerca de la pared
-fig = Figure();
-ax = Axis(fig[1,1], title = "speed, t = $(modelClose2wall.time |> x -> round(x; digits = 2)), x = $(modelClose2wall.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "|u|";
-modelClose2wall.fluidVelocity[desiredXid, :] .|> norm |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "fluid velocity");
-modelClose2wall.particles[1].nodeVelocity[desiredXid, :] .|> norm |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
-component = 1
-fig = Figure();
-ax = Axis(fig[1,1], title = "x-component of velocity, t = $(modelClose2wall.time |> x -> round(x; digits = 2)), x = $(modelClose2wall.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "u_x";
-modelClose2wall.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "fluid velocity");
-modelClose2wall.particles[1].nodeVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
-component = 2
-fig = Figure();
-ax = Axis(fig[1,1], title = "y-component of velocity, t = $(modelClose2wall.time |> x -> round(x; digits = 2)), x = $(modelClose2wall.spaceTime.x[desiredXid])");
-ax.xlabel = "y"; ax.ylabel = "u_y ";
-modelClose2wall.fluidVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "fluid velocity");
-modelClose2wall.particles[1].nodeVelocity[desiredXid, :] .|> (v -> v[component]) |> v -> lines!(ax, modelClose2wall.spaceTime.x, v, label = "tangential velocity (particle)");
-axislegend(ax, position = :rt)
-save_jpg("figs/$(today())/LBM figure $(Time(now()))", fig)
+mv("output.lbm", "$(outputDir)/L")
+
+#= ==========================================================================================
+=============================================================================================
+R - cerca de la pared
+=============================================================================================
+========================================================================================== =#
+
+modelClose2wall = modelInit(;
+    x = x,
+    relaxationTimeRatio = relaxationTimeRatio,
+    walledDimensions = walledDimensions,
+    isFluidCompressible = isFluidCompressible,
+    solidNodes = solidNodes,
+    saveData = true,
+);
+addBead!(modelClose2wall;
+    massDensity = massDensity,
+    radius = radius,
+    position = position,
+    coupleTorques = coupleTorques,
+    coupleForces = coupleForces,
+    angularVelocity = angularVelocity,
+    scheme = :psm
+);
+
+@time LBMpropagate!(modelClose2wall; simulationTime = simulationTime, verbose = true);
+@time plotMassDensity(modelClose2wall);
+@time plotFluidVelocity(modelClose2wall);
+
+mv("output.lbm", "$(outputDir)/R")
+
