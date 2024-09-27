356711f3d2289205114b703a1bf030a530689a68
David Correa <david.correa.bsc@gmail.com>
2024-09-27 16:24:27 -0600

    reorganized everything

---
+++ 7 testing image particle/src/params.jl
@@ -0,0 +1,27 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+x = range(-2, stop = 2, length = 201);
+walledDimensions = [1,2];
+
+# fluid
+relaxationTimeRatio = 1.1;
+isFluidCompressible = false;
+
+# wall
+solidNodes = [j < -0.7 for i in x, j in x];
+
+# particle
+massDensity = 1.5;
+radius = 0.4;
+position = [0., 0];
+coupleTorques = false;
+coupleForces = false;
+angularVelocity = 0.01;
+
+# simulation
+simulationTime = 30;
