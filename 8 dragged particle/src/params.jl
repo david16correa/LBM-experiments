4e99ddb8f15dbd16ca136fa5c7cacfb45ecd822d
David Correa <david.correa.bsc@gmail.com>
2024-09-27 16:25:07 -0600

    reorganized everything

--- 8 dragged particle/src/params.jl
+++ 8 dragged particle/src/params.jl
@@ -5,23 +5,26 @@ params
 ========================================================================================== =#
 
 # space
-x = range(-2, stop = 2, length = 201);
-walledDimensions = [1,2];
+x = range(-1, stop = 1, length = 200);
 
 # fluid
 relaxationTimeRatio = 1.1;
-isFluidCompressible = false;
+isFluidCompressible = true;
 
 # wall
-solidNodes = [j < -0.7 for i in x, j in x];
+speed = 0.06
+solidNodes = [-0.9 > i || i > 0.9 || -0.9 > j || j > 0.9 for i in x, j in x];
+solidNodeVelocity = [i > 0.9 ? [0, speed] : i < -0.9 ? [0, -speed] : j > 0.9 ? [-speed, 0] : j < -0.9 ? [speed, 0] : [0., 0] for i in x, j in x];
+
 
 # particle
 massDensity = 1.5;
-radius = 0.4;
-position = [0., 0];
+radius = 0.1;
+position = [0, -0.5];
 coupleTorques = false;
 coupleForces = true;
-angularVelocity = 0.01;
+angularVelocity = 0.0;
+scheme = :psm;
 
 # simulation
 simulationTime = 60;
--- testing image particle/README.md
+++
@@ -1,3 +0,0 @@
-# Testing image particle
-
-The idea is to understand the effect of a nearby wall on a rotating (but otherwise stationary) particle. To do this, two experiments will be carried out, one with the wall and one without it. Subtracting the resulting fluid velocity for both cases should result in the fluid velocity produced by an "image particle".
