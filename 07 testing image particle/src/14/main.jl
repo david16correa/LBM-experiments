b60c94f6697961a305d5fd8100fd328330edbd40
David Correa <david.correa.bsc@gmail.com>
2024-10-23 07:55:43 -0600

    new test, and analysis

---
+++ 07 testing image particle/src/14/main.jl
@@ -0,0 +1,71 @@
+#= ==========================================================================================
+=============================================================================================
+preamble
+=============================================================================================
+========================================================================================== =#
+
+# ensuring the working directory is the correct one
+cd(@__DIR__)  # Change working directory to the location of the script
+srcPath = pwd() # save the path of the current working directory to ensure it's always used
+cd("../..") # change the working directory to the environment directoy
+envPath = pwd() # save the path of the current working directory to ensure it's always used
+cd(srcPath) # we return to the source path in order to save the output there
+
+# Environment
+using Pkg; Pkg.activate(envPath);
+
+# add LBMengine.jl to the environment
+#= ] dev ../../LBMengine.jl =#
+
+# packages
+using LBMengine
+
+# parameters
+include("$srcPath/params.jl")
+
+outputDir = "$envPath/data.lbm/src_14/"
+run(`mkdir -p $outputDir`)
+
+#= ==========================================================================================
+=============================================================================================
+main
+=============================================================================================
+========================================================================================== =#
+
+println("initializing model for swimming particle..."); flush(stdout);
+
+model = modelInit(;
+    x = x,
+    viscosity = viscosity,
+    walledDimensions = walledDimensions,
+    isFluidCompressible = isFluidCompressible,
+    collisionModel = :trt,
+    saveData = true
+);
+addBead!(model;
+    massDensity = massDensity,
+    radius = radius,
+    position = position,
+    coupleTorques = coupleTorques,
+    coupleForces = coupleForces,
+    angularVelocity = angularVelocity,
+    scheme = :ladd,
+);
+
+println("running simulation..."); flush(stdout);
+
+@time LBMpropagate!(model; verbose = true, simulationTime = simulationTime, ticksBetweenSaves = ticksBetweenSaves);
+
+
+println("plotting the mass density and fluid velocity..."); flush(stdout);
+
+plotMassDensity(model); plotFluidVelocity(model);
+
+println("finding stress tensor..."); flush(stdout);
+
+@time sigma = viscousStressTensor(model)
+writeTensor(model, sigma, "stressTensor")
+
+println("moving data..."); flush(stdout);
+
+mv("$srcPath/output.lbm", "$(outputDir)/swimming")
---
+++ 07 testing image particle/src/14/params.jl
@@ -0,0 +1,27 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xmax = 15 # μm
+x = range(-xmax, stop = xmax, step = 0.1);
+walledDimensions = [1,2];
+
+# fluid
+# water viscosity ≡ μ = 0.890 mPa s, water density ≡ ρ = 1000 kg/m³ → water kinematic shear viscosity ≡ ν ≡ μ/ρ = 0.890 (μm)²/(ms),
+viscosity = 0.890 # (μm)²/(μs)
+isFluidCompressible = false;
+
+# particle
+massDensity = 1.; # water density units
+radius = 1.0; # μm
+position = [0, 1.5-xmax]; # μm
+coupleTorques = false;
+coupleForces = true;
+angularVelocity = 1e-6; # rad/μs
+
+# simulation
+simulationTime = 20e3; # μs → 0.02 s = 20 ms
+ticksBetweenSaves = 2e3; # ticksBetweenSaves = 2,000, 0.1 μs per tick → 0.2 ms between saves → 100 ticks saved
