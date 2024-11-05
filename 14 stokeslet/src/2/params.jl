290a94dd22ea043f291b5fe6ced0e74297393d11
David Correa <david.correa.bsc@gmail.com>
2024-11-04 23:34:54 -0600

    new tests
    vortex formation still present, it shouldn't be

---
+++ 14 stokeslet/src/2/params.jl
@@ -0,0 +1,25 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xmax = 50 # mm
+x = range(-xmax, stop = xmax, step = 0.1);
+walledDimensions = [];
+
+# fluid
+# water viscosity ≡ μ = 0.890 mPa s, water density ≡ ρ = 1000 kg/m³ → water kinematic shear viscosity ≡ ν ≡ μ/ρ = 0.890 (mm)²/(s),
+# this time I'm using unitary viscosity in the order of water viscosity
+viscosity = 1 # (mm)²/(s)
+isFluidCompressible = false;
+
+# stokeslet
+radius = 0.5; # mm
+position = [0., 0];
+force = [1e-3, 0.]; # mass density units * mm/s²
+
+# simulation
+simulationTime = 150; # s
+ticksBetweenSaves = 100 |> snapshots -> simulationTime / step(x) / snapshots |> round |> Int64; # (about) 100 snapshots are saved
---
+++ 14 stokeslet/src/3/main.jl
@@ -0,0 +1,60 @@
+#= this script is intended to be run using:
+julia main.jl > out.out 2>&1
+=#
+#= ==========================================================================================
+=============================================================================================
+preamble
+=============================================================================================
+========================================================================================== =#
+
+# directories and paths
+cd(@__DIR__)  # Change working directory to the location of the script
+srcPath = pwd() # save the source path
+cd("../..") # change the working directory to the environment directoy
+envPath = pwd() # save the environment path
+cd(srcPath) # change back to the source path in order to write the output there
+src_n = findall(char -> char == '/', srcPath)[end] |> id -> srcPath[id+1:end] # the src number is saved
+
+outputDir = "$envPath/data.lbm"
+run(`mkdir -p $outputDir`)
+
+# Environment
+using Pkg; Pkg.activate(envPath)
+
+# packages
+using LBMengine
+
+# parameters
+include("$srcPath/params.jl")
+
+#= ==========================================================================================
+=============================================================================================
+main
+=============================================================================================
+========================================================================================== =#
+
+println("initializing model..."); flush(stdout);
+
+stokeslet = [ ((i-position[1])^2 + (j - position[2])^2 <= radius^2) ? force : [0., 0]  for i in x, j in x]
+
+model = modelInit(;
+    x = x,
+    viscosity = viscosity,
+    walledDimensions = walledDimensions,
+    isFluidCompressible = isFluidCompressible,
+    forceDensity = stokeslet,
+    saveData = true
+);
+
+println("running simulation..."); flush(stdout);
+@time LBMpropagate!(model; verbose = true, simulationTime = simulationTime, ticksBetweenSaves = ticksBetweenSaves);
+#
+println("plotting the mass density and fluid velocity..."); flush(stdout);
+plotMassDensity(model); plotFluidVelocity(model);
+
+println("finding stress tensor..."); flush(stdout);
+@time sigma = viscousStressTensor(model)
+writeTensor(model, sigma, "stressTensor")
+
+println("moving data..."); flush(stdout);
+mv("$srcPath/output.lbm", "$(outputDir)/$(src_n)")
