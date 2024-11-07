7e441d998f3e26f099c0a02d7fbc5c49bb90d011
David Correa <david.correa.bsc@gmail.com>
2024-11-07 11:50:44 -0600

    reorganized stuff, and added two more tests

---
+++ 14 stokeslet/src/07/params.jl
@@ -0,0 +1,22 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xmax = 50 # mm
+x = range(-xmax, stop = xmax, step = 0.1);
+walledDimensions = [1,2];
+
+# fluid
+viscosity = 10 # (mm)²/(s)
+isFluidCompressible = false;
+
+# stokeslet
+position = [0., 0];
+force = [1e-3, 0.]; # mass density units * mm/s²
+
+# simulation
+simulationTime = 100; # s
+ticksBetweenSaves = 100 |> snapshots -> simulationTime / step(x) / snapshots |> round |> Int64; # (about) 100 snapshots are saved
