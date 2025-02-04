b620d1305cfdd1de652a3394e3e0304a33e1da6d
David Correa <david.correa.bsc@gmail.com>
2025-02-04 16:36:56 -0600

    new experiments
    now I use a box instead of pbcs

---
+++ 16 squirmers/src/03/params.jl
@@ -0,0 +1,26 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xlims = (-50, 50); # μm
+latticeParameter = 0.1; # μm
+walledDimensions = [1,2];
+
+# fluid
+relaxationTimeRatio = 1.1;
+isFluidCompressible = false;
+
+# squirmer
+radius = 4;
+slipSpeed = 1e-3;
+beta = -5;
+coupleTorques = false;
+coupleForces = false;
+scheme = :ladd;
+
+# simulation
+simulationTime = 1e3; # μs
+ticksBetweenSaves = 100 |> snapshots -> simulationTime / latticeParameter / snapshots |> round |> Int64; # (about) 100 snapshots are saved
