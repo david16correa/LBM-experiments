7db6760588849960371617471f9efb5465cf2e11
David Correa <david.correa.bsc@gmail.com>
2025-02-06 21:41:12 -0600

    new tests
    attempted to reproduce the systems shown in Kuron's work

---
+++ 16 squirmers/src/08/params.jl
@@ -0,0 +1,25 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xlims = (-20, 20); # μm
+latticeParameter = 0.1; # μm
+
+# fluid
+relaxationTimeRatio = 1.1;
+isFluidCompressible = false;
+
+# squirmer
+radius = 4;
+slipSpeed = 1e-3;
+beta = 5;
+coupleTorques = false;
+coupleForces = false;
+scheme = :ladd;
+
+# simulation
+simulationTime = 5e3; # μs
+ticksBetweenSaves = 10 |> snapshots -> simulationTime / latticeParameter / snapshots |> round |> Int64; # (about) 10 snapshots are saved
