41831d441c2f6bbfa878b3cdacbd76bd0e836df4
David Correa <david.correa.bsc@gmail.com>
2025-02-12 12:09:17 -0600

    new experiments
    :psm used to make squirmers swim

---
+++ 16 squirmers/src/10/params.jl
@@ -0,0 +1,26 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xlims = (-20, 20); # μm
+latticeParameter = 0.1; # μm
+
+# fluid
+relaxationTimeRatio = 1.1;
+isFluidCompressible = false;
+
+# squirmer
+radius = 4;
+position = [-6., 0];
+slipSpeed = 1e-3;
+beta = 0;
+coupleTorques = false;
+coupleForces = true;
+scheme = :psm;
+
+# simulation
+simulationTime = 10e3; # μs
+ticksBetweenSaves = 10 |> snapshots -> simulationTime / latticeParameter / snapshots |> round |> Int64; # (about) 10 snapshots are saved
