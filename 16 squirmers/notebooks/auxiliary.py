de5fb1d4bc537b0582f4af846d50f89651d996a1
David Correa <david.correa.bsc@gmail.com>
2025-02-19 16:00:40 -0600

    better figures

--- 16 squirmers/notebooks/auxiliary.py
+++ 16 squirmers/notebooks/auxiliary.py
@@ -254,3 +254,127 @@ def stressTensorOverview(df):
     #     ax.axhspan(-2, wallPosition, color='gray', alpha=0.1)  # Shade the wall
     
     return fig, axes
+
+def fluidOverview2(df1, df2, title):
+    fig, axes = plt.subplots(1, 2, figsize = (6,2.7))
+
+    fig.suptitle(title, fontsize=16, y=1.05)
+    fig.subplots_adjust(right=0.875)  # Adjust the right space to make room for the colorbar
+
+    # Create a meshgrid for plotting
+    x_unique = df1['coordinate_x'].unique()
+    y_unique = df1['coordinate_y'].unique()
+    X, Y = np.meshgrid(x_unique, y_unique)
+    
+    xmax = x_unique.max()
+    xmin = x_unique.min()
+    ymax = y_unique.max()
+    ymin = y_unique.min()
+
+    # first plot
+    axes[0].set_title("particle frame")
+    upperLim = 1e-3 * 2/3 + 1e-4 # maximum fluid speed should be 2/3*1e-3
+    lowerLim = 0.
+    cmap = plt.get_cmap('cividis')
+    norm = plt.Normalize(lowerLim, upperLim)
+
+    axes[0].streamplot(X, Y, 
+        df1.fluidVelocity_x.unstack().values.transpose(),
+        df1.fluidVelocity_y.unstack().values.transpose(),
+        density=0.85, linewidth=0.5, color="black",
+    )
+    
+    
+    axes[0].pcolormesh(X, Y,
+        np.sqrt(df1.fluidVelocity_x**2 + df1.fluidVelocity_y**2).unstack().values.transpose(),
+        vmin=lowerLim,
+        vmax=upperLim,
+        cmap=cmap, alpha = 0.85
+    );
+
+    axes[0].set_xticks([xmin, 0,xmax])
+    axes[0].set_xticklabels([f'{xmin:.0f}', 0,f'{xmax:.0f}'])
+    axes[0].set_xlabel("$x ~ (\\mathrm{\\mu m})$")
+    axes[0].set_yticks([ymin, 0, ymax])
+    axes[0].set_yticklabels([f'{ymin:.0f}', 0,f'{ymax:.0f}'])
+    axes[0].set_ylabel("$y ~ (\\mathrm{\\mu m})$")
+
+    cbar_ax = fig.add_axes([0.9, 0.15, 0.025, 0.7])  # [left, bottom, width, height] for the colorbar
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = fig.colorbar(sm, cax=cbar_ax, alpha=0.85)
+    cbar.set_label(label='$\\mathbf{u}$', fontsize=16)
+#     cbar.set_label(label='$|\\mathbf{u}| ~ \\left(10^{-3}~\\mathrm{mm}/\\mathrm{s}\\right)$', fontsize=16)
+    cbar.ax.tick_params(labelsize=15)
+
+    custom_ticks = np.array([lowerLim, upperLim/2, upperLim])
+    cbar.set_ticks(custom_ticks)
+    cbar.set_ticklabels([f'{tick:.4f}' for tick in custom_ticks])
+#     cbar.set_ticklabels(['$0$','$1.5$','$3$'])
+
+    # bottom plot
+    axes[1].set_title("lab frame")
+    axes[1].streamplot(X, Y, 
+        df2.fluidVelocity_x.unstack().values.transpose(),
+        df2.fluidVelocity_y.unstack().values.transpose(),
+        density=0.85, linewidth=0.5, color="black",
+    )
+    
+    
+    axes[1].pcolormesh(X, Y,
+        np.sqrt(df2.fluidVelocity_x**2 + df2.fluidVelocity_y**2).unstack().values.transpose(),
+        vmin=lowerLim,
+        vmax=upperLim,
+        cmap=cmap, alpha = 0.85
+    );
+
+    axes[1].set_xticks([xmin, 0,xmax])
+    axes[1].set_xticklabels([f'{xmin:.0f}', 0,f'{xmax:.0f}'])
+    axes[1].set_xlabel("$x ~ (\\mathrm{\\mu m})$")
+    axes[1].set_yticks([ymin, 0, ymax])
+    axes[1].tick_params(labelleft=False)
+    
+    return fig, axes
+
+def stressTensorOverview(df):
+    # setting up stuff
+    fig, axes = plt.subplots(2,2,figsize = (10,10))
+
+    # Add a title for the whole figure
+    fig.suptitle("$\\sigma_{ij}$", fontsize=16)
+
+    # Create a meshgrid for plotting
+    x_unique = df['coordinate_x'].unique()
+    y_unique = df['coordinate_y'].unique()
+    X, Y = np.meshgrid(x_unique, y_unique)
+
+    cs = []
+
+    # plotting
+    c = axes[0,0].pcolormesh(X,Y,df["component_xx"].unstack().values.transpose()); cs.append(c);
+    axes[0,0].set_title("$\\sigma_{xx}$")
+
+    c = axes[0,1].pcolormesh(X,Y,df["component_xy"].unstack().values.transpose()); cs.append(c);
+    axes[0,1].set_title("$\\sigma_{xy}$")
+
+    c = axes[1,0].pcolormesh(X,Y,df["component_yx"].unstack().values.transpose()); cs.append(c);
+    axes[1,0].set_title("$\\sigma_{yx}$")
+
+    c = axes[1,1].pcolormesh(X,Y,df["component_yy"].unstack().values.transpose()); cs.append(c);
+    axes[1,1].set_title("$\\sigma_{yy}$")
+
+
+    # for ax in axes.flat:
+    for i in range(4):
+        ax = axes.flatten()[i]
+        c = cs[i]
+        ax.set_aspect('equal')
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")    
+        cbar = fig.colorbar(c, ax=ax, shrink=.8)
+        cbar.ax.yaxis.set_major_formatter(formatter)
+        cbar.ax.yaxis.get_offset_text().set_fontsize(10)  # Adjust the font size if necessary
+    #     ax.axhline(wallPosition, color = "k", alpha = 0.1)
+    #     ax.axhspan(-2, wallPosition, color='gray', alpha=0.1)  # Shade the wall
+    
+    return fig, axes
