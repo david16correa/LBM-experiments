7ae72d5c8fa2757c00fe6b13c9285dbbc96592ae
David Correa <david.correa.bsc@gmail.com>
2025-04-23 12:42:42 -0600

    new experiment

---
+++ 19 articulated paramagnetic rod/src/04/params.jl
@@ -0,0 +1,35 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xlims = (-50, 50); # μm
+dims = 2;
+
+# fluid
+viscosity = 1; # (μm)²/(μs)
+
+# beads
+nBeads = 5; # must be odd; otherwise the rod won't have a clear fulcrum
+radius = 2; # μm
+# all beads are placed on the x axis, evenly distributed around the origin
+xs = range(start=0, step=2.3*radius, length=nBeads/2|>ceil|>Integer) |> collect |> v -> cat(v, -v[2:end]; dims = 1) |> unique |> sort # μm; 
+coupleForces = true;
+coupleTorques = true;
+
+# linear bonds
+bondPairs = [(id, id+1) for id in 1:nBeads-1]
+stiffness = 1 # mN/m
+
+# polar bonds
+bondTriplets = [(1,2,3), (3,4,5)]
+equilibriumAngle = pi # rad
+
+# magnetic dipoles
+magneticField = [0, 5] # mT
+
+# simulation
+simulationTime = 40e3; # μs
+ticksSaved = 100;
