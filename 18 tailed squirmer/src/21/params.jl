75ae21e14dc9a936da02173e9c5cd3e29ebf8799
David Correa <david.correa.bsc@gmail.com>
2025-04-25 15:09:19 -0600

    many more experiments

---
+++ 18 tailed squirmer/src/21/params.jl
@@ -0,0 +1,33 @@
+#= ==========================================================================================
+=============================================================================================
+params
+=============================================================================================
+========================================================================================== =#
+
+# space
+xlims = (-100,100); # μm
+ylims = (-50,50); # μm
+dims = 2;
+
+# fluid
+viscosity = 1; # (μm)²/(μs)
+
+# beads
+radius = 3; # μm
+nBeads = 15
+xs = range(stop=0, step=radius*2.3, length=nBeads) |> collect # μm
+coupleForces = true;
+coupleTorques = true;
+
+# squirmer
+swimmingSpeed = 1e-3;
+beta = 0;
+swimmingDirection = [1,0];
+
+# bonds
+bondPairs = [(id, id+1) for id in 1:nBeads-1]
+stiffness = 1 # mN/m
+
+# simulation
+simulationTime = 5e3; # μs
+ticksSaved = 10;
